8fddff362ead5ab0ad2bfd3bdce503db7dec3b2a
gaurav.pawar <pawargaurav1256@gmail.com>
2025-08-22 16:21:22 +0530

    calculation using string convert into integer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import React, { useState } from "react";
 
 export default function App() {
-    const [number, setNumber] = useState();
-    const[showBlock,setShowBlock]= useState(false);
-    // const [submittedNumber, setSubmittedNumber] = useState('');
-    // const [submittedImage, setSubmittedImage] = useState('');
+    const [number1, setNumber1] = useState();
+    const [number2, setNumber2] = useState();
+    const[addition, setAddition]= useState();
+    const[substraction, setSubstraction]=useState();
+    const[multiplication,setMultiplication]=useState();
+    const[division,setDivision]=useState();
 
-    function handleAgeChange(event) {
-        setNumber(event.target.value);
+    function handleNumber1Change(event){
+        setNumber1(parseInt(event.target.value))
     }
-    function onSubmitClick(){
-        setShowBlock(true)
+    function handleNumber2Change(event){
+        setNumber2(parseInt(event.target.value))
     }
+    function onSubmit(){
+        setAddition(number1+number2);
+        setSubstraction(number1-number2);
+        setMultiplication(number1*number2);
+        setDivision(number1/number2);
+    }
+
     
     return (
         <div>
-            <input name="text" value={number} onChange={handleAgeChange} />
-            
-            <button onClick={onSubmitClick}>Submit</button>
-            {showBlock && <div>
-                <p>{number}</p>
-                <img src="https://images.unsplash.com/photo-1526779259212-939e64788e3c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D" width="300" />
-            </div>}
+            <input name="text" value={number1} onChange={handleNumber1Change}/>
+            <input name="text" value={number2} onChange={handleNumber2Change}/>
+            <button onClick={onSubmit}>calculate</button>
+            {
+                addition &&<h1>addition of {number1} and {number2} is:{addition}</h1>}
+             {   substraction &&<h1>substraction of {number1} and {number2} is:{substraction}</h1>}
+             {   multiplication &&<h1>multiplication of {number1} and {number2} is:{multiplication}</h1>}
+             {   division &&<h1>division of {number1} and {number2} is:{division}</h1>
+
+            }
         </div>
     );
 }
