7d2577d17a3eab45f2e48d5ea9107a9dc3570bcd
gaurav.pawar <pawargaurav1256@gmail.com>
2025-08-22 15:25:33 +0530

    show name and image click on submit button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
 
 export default function App() {
-    const [age, setAge] = useState();
-    function handleagechange(event) {
-        setAge(event.target.value)
-    }
+    const [number, setNumber] = useState();
+    const[showBlock,setShowBlock]= useState(false);
+    // const [submittedNumber, setSubmittedNumber] = useState('');
+    // const [submittedImage, setSubmittedImage] = useState('');
 
+    function handleAgeChange(event) {
+        setNumber(event.target.value);
+    }
+    function onSubmitClick(){
+        setShowBlock(true)
+    }
+    
     return (
         <div>
-            <input name="age" value={age} onChange={handleagechange} />
-            <div>{age}</div>
+            <input name="text" value={number} onChange={handleAgeChange} />
+            
+            <button onClick={onSubmitClick}>Submit</button>
+            {showBlock && <div>
+                <p>{number}</p>
+                <img src="https://images.unsplash.com/photo-1526779259212-939e64788e3c?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZnJlZSUyMGltYWdlc3xlbnwwfHwwfHx8MA%3D%3D" width="300" />
+            </div>}
         </div>
-    )
+    );
 }
